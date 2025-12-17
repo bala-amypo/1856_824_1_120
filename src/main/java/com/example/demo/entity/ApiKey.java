2fafdcc760c7bf1e802615b39f6f49a95f3c1dc9
bala-amypo <bala@amypo.in>
2025-12-17 08:48:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/ApiKey.java
+++ src/main/java/com/example/demo/entity/ApiKey.java
@@ -1,11 +1,23 @@
 package com.example.demo.entity;
 
+import jakarta.presistence.Entity;
+import jakarta.presistence.id;
+import jakarta.presistence.GeneratedValue;
+import jakarta.presistence.GenerationType;
+import jakarta.presistence.Column;
+import jakarta.presistence.ManyTo
+
+@Entity
 public class ApiKey{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.INDENTITY)
     private Long id;
+    @Column(unique=true)
     private String keyValue;
     private Long ownerld;
     private String plan;
     private Boolean active;
     private =new createdAt;
-    private =new 
+    private =new updatedAt;
 }
