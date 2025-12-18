c3803cf065d3c16d027c1120f17e37ff7967d218
bala-amypo <bala@amypo.in>
2025-12-18 06:41:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/ApiUsageLog.java
+++ src/main/java/com/example/demo/entity/ApiUsageLog.java
@@ -3,7 +3,10 @@ package com.example.demo.entity;
 @Entity
 public class ApiUsageLog{
     @Id
-    @GenratedValue
+    @GenratedValue(strategy = GenerationType.IDENTITY )
     private Long id;
+    private String apiKey;
+    private String 
+
 
 }
