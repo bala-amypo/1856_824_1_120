eb7a3eb70dd2603323c4861b0f9b6708b74f9816
bala-amypo <bala@amypo.in>
2025-12-18 04:46:18 +0000

    User commit

--- src/main/java/com/example/demo/entity/QuotaPlan.java
+++ src/main/java/com/example/demo/entity/QuotaPlan.java
@@ -4,4 +4,14 @@ package com.example.demo.entity;
 
 public class QuotaPlan{
 
+    @Id
+    @GeneratedValue()
+    private Long id;
+    @column(unique=true);
+    private String planName;
+    private Integer dailyLimit;
+    private String description;
+    private Boolean active;
+
+
 }
