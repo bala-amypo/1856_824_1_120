b6bf05d75b60569526274752cc444fca542099dc
bala-amypo <bala@amypo.in>
2025-12-19 05:03:30 +0000

    User commit

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -1,2 +1,7 @@
 package com.example.demo.service;
-import com.example.demo.entity.U
+import com.example.demo.entity.UserAccount;
+import jakarta.persistence.sevice;
+
+public interface AuthService{
+    public UserAccount register(RegisterRequestDto request);
+}
