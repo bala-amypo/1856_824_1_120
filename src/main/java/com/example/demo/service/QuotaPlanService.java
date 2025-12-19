60c984dde6bbadf6b2d7633cdbf30ee4e1b0d4fa
bala-amypo <bala@amypo.in>
2025-12-19 04:36:30 +0000

    User commit

--- src/main/java/com/example/demo/service/QuotaPlanService.java
+++ src/main/java/com/example/demo/service/QuotaPlanService.java
@@ -1,3 +1,19 @@
 package com.example.demo.service;
-
 import com.example.demo.entity.QuotaPlan;
+import jakarta.persistence.service;
+
+
+public interface ApiKeyService{
+    public ApiKey createApiKey(ApiKey key);
+
+    public ApiKey updateApiKey(Long id, ApiKey key);
+
+    public ApiKey getApiKeyById(Long id);
+
+    public ApiKey getApiKeyByValue(String keyValue);
+
+    public ApiKey getAllKeys();
+
+    public ApiKey deactivateApiKey(Long id);
+    
+}
