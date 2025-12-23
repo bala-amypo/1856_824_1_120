720448edc81b645b9008bc610701dc802d8d8662
bala-amypo <bala@amypo.in>
2025-12-23 04:46:19 +0000

    User commit

--- src/main/java/com/example/demo/service/ApiKeyService.java
+++ src/main/java/com/example/demo/service/ApiKeyService.java
@@ -1,19 +1,19 @@
-// package com.example.demo.service;
+package com.example.demo.service;
 
-// import com.example.demo.entity.ApiKey;
-// import java.util.List;
+import com.example.demo.entity.ApiKey;
+import java.util.List;
 
-// public interface ApiKeyService {
+public interface ApiKeyService {
 
-//     ApiKey createApiKey(ApiKey apiKey);
+    ApiKey createApiKey(ApiKey key);
 
-//     ApiKey updateApiKey(Long id, ApiKey apiKey);
+    ApiKey updateApiKey(Long id, ApiKey key);
 
-//     ApiKey getApiKeyById(Long id);
+    ApiKey getApiKeyById(Long id);
 
-//     ApiKey getApiKeyByValue(String keyValue);
+    ApiKey getApiKeyByValue(String keyValue);
 
-//     List<ApiKey> getAllApiKeys();
+    List<ApiKey> getAllApiKeys();
 
-//     void deactivateApiKey(Long id);
-// }
+    void deactivateApiKey(Long id);
+}
