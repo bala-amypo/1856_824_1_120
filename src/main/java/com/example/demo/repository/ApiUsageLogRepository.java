357b09bf7b5ff2d43b9bee78aca42ae3db5831df
bala-amypo <bala@amypo.in>
2025-12-23 04:25:18 +0000

    User commit

--- src/main/java/com/example/demo/repository/ApiUsageLogRepository.java
+++ src/main/java/com/example/demo/repository/ApiUsageLogRepository.java
@@ -1,8 +1,18 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.ApiUsageLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ApiUsageLogRepository extends JpaRepository<ApiUsageLog, Long>{
+import java.util.List;
 
+public interface ApiUsageLogRepository extends JpaRepository<ApiUsageLog, Long> {
+
+    List<ApiUsageLog> findByApiKeyId(Long apiKeyId);
+
+    @Query("select l from ApiUsageLog l where l.apiKey.id = :keyId and date(l.timestamp) = current_date")
+    List<ApiUsageLog> findTodayUsage(Long keyId);
+
+    @Query("select count(l) from ApiUsageLog l where l.apiKey.id = :keyId and date(l.timestamp) = current_date")
+    Long countTodayRequests(Long keyId);
 }
