41f597ee5fb53bec3506824a902d1dd915c5621d
bala-amypo <bala@amypo.in>
2025-12-26 06:27:09 +0000

    User commit

--- src/main/java/com/example/demo/dto/ApiKeyUsageSummaryDto.java
+++ src/main/java/com/example/demo/dto/ApiKeyUsageSummaryDto.java
@@ -1,6 +1,31 @@
-// package com.example.demo.dto;
+package com.example.demo.dto;
 
+public class ApiKeyUsageSummaryDto {
 
-// public class ApiKeyUsageSummaryDto{
-    
-// }
+    private Long apiKeyId;
+    private Long totalRequestsToday;
+
+    public ApiKeyUsageSummaryDto() {
+    }
+
+    public ApiKeyUsageSummaryDto(Long apiKeyId, Long totalRequestsToday) {
+        this.apiKeyId = apiKeyId;
+        this.totalRequestsToday = totalRequestsToday;
+    }
+
+    public Long getApiKeyId() {
+        return apiKeyId;
+    }
+
+    public void setApiKeyId(Long apiKeyId) {
+        this.apiKeyId = apiKeyId;
+    }
+
+    public Long getTotalRequestsToday() {
+        return totalRequestsToday;
+    }
+
+    public void setTotalRequestsToday(Long totalRequestsToday) {
+        this.totalRequestsToday = totalRequestsToday;
+    }
+}
--- src/main/java/com/example/demo/security/JwtUtil.java
+++ src/main/java/com/example/demo/security/JwtUtil.java
@@ -1,5 +1,42 @@
-// package com.example.demo.security;
+package com.example.demo.security;
 
-// public class JwtUtil{
-    
-// }
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.Map;
+
+public class JwtUtil {
+
+    private final String SECRET = "secretkey123456";
+
+    public String generateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + getExpirationMillis()))
+                .signWith(SignatureAlgorithm.HS256, SECRET)
+                .compact();
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String getUsername(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isTokenValid(String token, String username) {
+        return username.equals(getUsername(token));
+    }
+
+    public long getExpirationMillis() {
+        return 1000 * 60 * 60; // 1 hour
+    }
+}
